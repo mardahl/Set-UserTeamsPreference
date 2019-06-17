60194d1ef4dd64525322d49beda3fe556941ae91
Michael Mardahl <iphase@iphase.dk>
2019-06-17 18:08:19 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Set-UserTeamsPreference
+
+A PowerShell script to modify a users preferences in the Microsoft Teams Desktop Client.
+
+* Thanks go out to James Rankin https://james-rankin.com/articles/microsoft-teams-on-citrix-xenapp/ for the code inspiration. *
+
+## Usage
+
+This script works by accepting commandline switches, defining a switch enforces the option, else sets it back to default.
+
+- openAsHidden
+-- Minimize Teams to Systray on autostart
+- DisableAutoStart
+-- Disables automatic launch of the Teams Client
+- registerAsIMProvider
+-- Registers the Teams Client as an Instant Message client
+- exitOnClose
+-- Exists the teams client on closing the window, instead of running it in the background
+
+
+# Deploy with Intune
+This script was made to be deployed as a win32app by Intune.
+This is also the reason for commandline parameters.
+So that you don't have to repackage the script each time you want to change preferences, or if you wish to have two sets of preferences for different groups of users - the same .intune package can be used.
+
+You will need to set a dependecy rule that ensures that Teams is installed as the user before installation.
+As this script will fail if the configuration file is not present in "%APPDATA%\Microsoft\Teams\desktop-config.json"
+
+You could also make a custom Teams installer script which runs the script after installation of the client.
+
+A transcript is saved to the users TEMP folder, for easier debugging than the Intune logs.
+"%TEMP%\Set-UserTeamsPreference_Script-log.txt"
+
+
+
---
+++ Set-UserTeamsPreference.ps1
@@ -0,0 +1,89 @@
+#Requires -Version 5.0
+<#
+.SYNOPSIS
+Set a users Teams Preference
+.DESCRIPTION
+This script will change the the users Teams configuration file, so the client behaves as requested by the administrator.
+.EXAMPLE
+Run the script as is, to set defaults
+.EXAMPLE
+Make teams start minimized to systray
+Set-UserTeamsPreference.ps1 -openAsHidden
+.NOTES
+NAME: Set-UserTeamsPreference.ps1
+VERSION: 1906a
+PREREQ:  Make sure Teams is installed as the user before running!
+Coded for readability!
+.COPYRIGHT
+@michael_mardahl / https://www.iphase.dk
+Licensed under the MIT license.
+Please credit me if you fint this script useful and do some cool things with it.
+Thanks go out to James Rankin https://james-rankin.com/articles/microsoft-teams-on-citrix-xenapp/ for the code inspiration.
+#>
+
+# Parameters that are accepted by the script, none are required.
+PARAM(
+    [switch]$openAsHidden,
+    [switch]$disableAutoStart,
+    [switch]$registerAsIMProvider,
+    [switch]$exitOnClose
+)
+
+# Logging any errors to the users temp folder
+Start-Transcript -Path "$env:TEMP\Set-UserTeamsPreference_Script-log.txt"
+Write-Output "Modifying the users Teams client..."
+
+# Enabling verbose output for our log
+$VerbosePreference = "Continue"
+
+# The users Teams configuration file
+$configFile = "$ENV:APPDATA\Microsoft\Teams\desktop-config.json"
+
+# Load the file data into a variable
+Write-Verbose "Fetching the contents of $configFile"
+$fileBuffer = Get-Content $configFile
+
+### Modifying the variable so Teams beahves as dictated by the specified commandline parameters (or lack thereof)
+
+# Hidden option
+if($openAsHidden) {
+    Write-Output '1 - Enabling the "openAsHidden" option...'
+    $fileBuffer = $fileBuffer -replace '"openAsHidden":false', '"openAsHidden":true'
+} else { 
+    Write-Output '1 - Disabling the "openAsHidden" option...'
+    $fileBuffer = $fileBuffer -replace '"openAsHidden":true', '"openAsHidden":false'
+}
+
+# Autostart option
+if($disableAutoStart) {
+    Write-Output '2 - Disabling the "openAtLogin" option...'
+    $fileBuffer = $fileBuffer -replace '"openAtLogin":true', '"openAtLogin":false'
+} else { 
+    Write-Output '2 - Enabling the "openAtLogin" option...'
+    $fileBuffer = $fileBuffer -replace '"openAtLogin":false', '"openAtLogin":true'
+}
+
+# IM Provider option
+if($registerAsIMProvider) {
+    Write-Output '3 - Enabling the "registerAsIMProvider" option...'
+    $fileBuffer = $fileBuffer -replace '"registerAsIMProvider":false', '"registerAsIMProvider":true'
+} else {
+    Write-Output '3 - Disabling the "registerAsIMProvider" option...'
+    $fileBuffer = $fileBuffer -replace '"registerAsIMProvider":true', '"registerAsIMProvider":false'
+}
+
+# Program close behaviour option
+if($exitOnClose) {
+    Write-Output '4 - Disabling the "runningOnClose" option...'
+    $fileBuffer = $fileBuffer -replace '"runningOnClose":true', '"runningOnClose":false'
+} else {
+    Write-Output '4 - Enabling the "runningOnClose" option...'
+    $fileBuffer = $fileBuffer -replace '"runningOnClose":false', '"runningOnClose":true'
+}
+
+# Output our modified data back into the configuration file, force overwriting the contents
+Write-Verbose "Overwriting the contents of $configFile"
+$fileBuffer | Set-Content $configFile
+
+# Stopping the log
+Stop-Transcript
