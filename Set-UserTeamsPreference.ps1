48bc71fe2a281c4eccf9e8887a72056561a6c5f5
Michael Mardahl <iphase@iphase.dk>
2020-05-15 08:31:12 +0200

    Update Set-UserTeamsPreference.ps1
    New version that fixes the register IM provider option, thanks to imab.dk

--- Set-UserTeamsPreference.ps1
+++ Set-UserTeamsPreference.ps1
@@ -1,27 +1,35 @@
-#Requires -Version 5.0
 <#
 .SYNOPSIS
-Set a users Teams Preference
+    Set a users Teams Preference
 .DESCRIPTION
-This script will change the the users Teams configuration file, so the client behaves as requested by the administrator.
-.EXAMPLE
-Run the script as is, to set defaults
-.EXAMPLE
-Make teams start minimized to systray and register as an IM provider
-Set-UserTeamsPreference.ps1 -openAsHidden -registerAsIMProvider
+    This script will change the the users Teams configuration file, so the client behaves as requested by the administrator.
+.INPUTS
+    The following switches are available:
+    -openAsHidden          (minimizes Teams to systray on startup)
+    -disableAutoStart      (Stops Teams frostarting with the OS)
+    -registerAsIMProvider  (Makes Teams the default Instant Messaging Provider for the user)
+    -exitOnClose           (Makes the Teams client exit when the main windows is closed)
+.OUTPUTS
+    A log file called Set-UserTeamsPreference_Script-log.txt is written to the users own TEMP folder at each execution of the script (overwriting the previous log).
 .NOTES
-NAME: Set-UserTeamsPreference.ps1
-VERSION: 1906a
-PREREQ:  Make sure Teams is installed as the user before running!
-BUGS: 
-RegisterAsIMProvider switch only works to disable, not enable it seems.
-Coded for readability!
-.COPYRIGHT
-@michael_mardahl / https://www.iphase.dk
-Licensed under the MIT license.
-Please credit me if you fint this script useful and do some cool things with it.
-Thanks go out to James Rankin https://james-rankin.com/articles/microsoft-teams-on-citrix-xenapp/ for the code inspiration.
+    NAME: Set-UserTeamsPreference.ps1
+    VERSION: 2005a
+    PREREQ:  Make sure Teams is installed as the user before running!
+    Coded for readability!
+    COPYRIGHT:
+    @michael_mardahl / https://www.iphase.dk
+    Licensed under the MIT license.
+    Please credit me if you fint this script useful and do some cool things with it.
+    Thanks go out to James Rankin https://james-rankin.com/articles/microsoft-teams-on-citrix-xenapp/ for the code inspiration.
+    A belated thanks goes out to @mwbengtsson for figuring out the imProvider registry keys.
+.EXAMPLE
+    Run the script as is in the users context, to set defaults
+.EXAMPLE
+    Make teams start minimized to systray and register as an IM provider
+    Set-UserTeamsPreference.ps1 -openAsHidden -registerAsIMProvider
 #>
+
+#Requires -Version 5.0
 
 # Parameters that are accepted by the script, none are required.
 PARAM(
@@ -31,9 +39,7 @@ PARAM(
     [switch]$exitOnClose
 )
 
-# Logging any errors to the users temp folder
-Start-Transcript -Path "$env:TEMP\Set-UserTeamsPreference_Script-log.txt"
-Write-Output "Modifying the users Teams client..."
+#region Delcarations ######################################################################################
 
 # Enabling verbose output for our log 
 $VerbosePreference = "Continue"
@@ -41,14 +47,27 @@ $VerbosePreference = "Continue"
 # The users Teams configuration file
 $configFile = "$ENV:APPDATA\Microsoft\Teams\desktop-config.json"
 
+#endregion Declarations
+
+#region Execute ###########################################################################################
+
+# Logging any errors to the users temp folder
+Start-Transcript -Path "$env:TEMP\Set-UserTeamsPreference_Script-log.txt"
+Write-Output "Modifying the users Teams client..."
+
+#Closing Teams just in case (sorry users!)
+Write-Verbose "Killing any running Teams processes..."
+Stop-Process -Name Teams -Force -ErrorAction SilentlyContinue
+
 # Load the file data into a variable
 Write-Verbose "Fetching the contents of $configFile"
 try {
     $fileBuffer = Get-Content $configFile -ErrorAction Stop
 } 
 catch {
-    throw "Could not fetch the users Teams config file! Make sure Teams is installed!"
+    Write-Error "Could not fetch the users Teams config file! Make sure Teams is installed!"
     Stop-Transcript
+    exit 1
 }
 
 ### Modifying the variable so Teams beahves as dictated by the specified commandline parameters (or lack thereof)
@@ -75,9 +94,20 @@ if($disableAutoStart) {
 if($registerAsIMProvider) {
     Write-Verbose '3 - Enabling the "registerAsIMProvider" option...'
     $fileBuffer = $fileBuffer -replace '"registerAsIMProvider":false', '"registerAsIMProvider":true'
+    New-ItemProperty -Path "HKCU:\SOFTWARE\IM Providers" -Name DefaultIMApp -Value Teams -PropertyType STRING -Force -ErrorAction SilentlyContinue
 } else {
     Write-Verbose '3 - Disabling the "registerAsIMProvider" option...'
     $fileBuffer = $fileBuffer -replace '"registerAsIMProvider":true', '"registerAsIMProvider":false'
+    $imProviders = "HKCU:\SOFTWARE\IM Providers"
+    $teamsIMProvider = "HKCU:\SOFTWARE\IM Providers\Teams"
+    if (Test-Path -Path $teamsIMProvider) {
+        $previousDefaultIMApp = (Get-ItemProperty -Path $teamsIMProvider -Name PreviousDefaultIMApp -ErrorAction SilentlyContinue).PreviousDefaultIMApp
+        if ($previousDefaultIMApp) {
+            New-ItemProperty -Path $imProviders -Name DefaultIMApp -Value $previousDefaultIMApp -PropertyType STRING -Force
+        } else {
+            Remove-ItemProperty -Path $imProviders -Name DefaultIMApp -ErrorAction SilentlyContinue
+        }
+    }
 }
 
 # Program close behaviour option
@@ -91,7 +121,9 @@ if($exitOnClose) {
 
 # Output our modified data back into the configuration file, force overwriting the contents
 Write-Verbose "Overwriting the contents of $configFile"
-$fileBuffer | Set-Content $configFile
+$fileBuffer | Set-Content $configFile -Force
 
 # Stopping the log
 Stop-Transcript
+
+#endregion Execute
